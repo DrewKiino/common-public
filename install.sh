167d4a5cc49f179f2dcf9274f7c623eab9d6f2db
Andrew Aquino <andrewaquino118@gmail.com>
2020-02-07 00:47:31 -0800

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "alias boba_watch='sh ~/common-public/bin/watch.sh'" >>~/.bash_profile
 echo "alias boba_watch_file='sh ~/common-public/bin/watch_file.sh $1'" >>~/.bash_profile
+echo "alias boba_save_file='sh ~/common-public/bin/save_file.sh $1'" >>~/.bash_profile
 source ~/.bash_profile
 
 echo "done installing boba watch!"
