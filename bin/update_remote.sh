4f5c145569930047bc3b49d1465d24457c6ddaea
Andrew Aquino <andrewaquino118@gmail.com>
2020-02-06 23:39:30 -0800

    update

--- bin/update_remote.sh
+++ bin/update_remote.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # make sure we set the working directory into this one
-cd "$(dirname "$0")"
+
+DIR=$(dirname $0)
+BASE_DIR="${DIR%$STRIP_SUFFIX}"
+cd "$BASE_DIR"
+TARGET="$BASE_DIR/src"
+
+echo $(pwd)
 
 # then we can start running git commands
 git add . && \
